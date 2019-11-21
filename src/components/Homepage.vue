a84c1026b34d2c8fcd2fa0e46aea73fb44e90d42
henryc35 <henryc35@myumanitoba.ca>
2019-11-20 19:59:14 -0600

    Setup homepage

---
+++ src/components/Homepage.vue
@@ -0,0 +1,89 @@
+<template>
+  <div>
+    <b-row class="mt-4 mr-5 ml-5">
+      <b-col sm="4">
+        <b-card title="Door" class="card-style">
+          <b-card-text class="text-style">{{door}}</b-card-text>
+        </b-card>
+      </b-col>
+      <b-col sm="4">
+        <b-card class="card-style" title="Window">
+          <b-card-text class="text-style">Closed</b-card-text>
+        </b-card>
+      </b-col>
+      <b-col sm="4">
+        <b-card class="card-style" title="Humidity">
+          <b-card-text class="text-style">10%</b-card-text>
+        </b-card>
+      </b-col>
+    </b-row>
+    <b-row class="mt-3 ml-5 mr-5">
+      <b-col sm="4">
+        <b-card title="Temperature" class="card-style">
+          <b-card-text>
+            <b-card-text class="text-style">10 C</b-card-text>
+          </b-card-text>
+        </b-card>
+      </b-col>
+      <b-col sm="4">
+        <b-card class="card-style" title="Carbon Monoxide">
+          <b-card-text class="text-style">0</b-card-text>
+        </b-card>
+      </b-col>
+      <b-col sm="4">
+        <b-card class="card-style" title="Motion">
+          <b-card-text class="text-style">None</b-card-text>
+        </b-card>
+      </b-col>
+    </b-row>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+
+export default {
+  name: "homepage",
+  data: function() {
+      return {
+          door: "Closed"
+      }
+  },
+  created: function() {
+      this.getDoor()
+  },
+  methods: {
+      getDoor() {
+          let vm = this
+
+          return axios.get("http://127.0.0.1:8000/getDoor").then(res => {
+              vm.door = res.data
+          })
+      }
+  }
+};
+</script>
+
+<style scoped>
+.text-style {
+  margin-top: 25%;
+  font-size: 150%;
+}
+
+.header-style {
+  font-size: 100%;
+}
+
+.card-style {
+  background-color: #5792cf;
+  color: white;
+  margin-top: 10%;
+  margin-bottom: 35%;
+  margin-right: 0%;
+  margin-left: 0%;
+  height: 85%;
+  display: flex;
+  font-size: 200%;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import BootstrapVue from 'bootstrap-vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+ 
+Vue.use(VueAxios, axios)
+Vue.use(BootstrapVue)
+
+import './assets/app.css'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.config.productionTip = false
 
