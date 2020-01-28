77de71e5c43314539e87c2b5b83fd5da6efa6b68
henryc35 <henryc35@myumanitoba.ca>
2020-01-28 16:36:16 -0600

    Remove console statement

--- src/interceptor.js
+++ src/interceptor.js
@@ -17,7 +17,6 @@ export default function interceptors() {
     // Return a successful response back to the calling service
     return response;
   }, (error) => {
-    console.log(error.response)
     // Return any error which is not due to authentication back to the calling service
     if (error.response.status != 401) {
       return new Promise((resolve, reject) => {
